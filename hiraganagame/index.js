87eb86df54f43e925d11ef79e283d61df75effbb
Olive <141564336+xxOliveXyzxx@users.noreply.github.com>
2023-11-09 20:33:09 -0700

    Update index.js

--- hiraganagame/index.js
+++ hiraganagame/index.js
@@ -1,7 +1,13 @@
-String[][] quiz = {
+var quizone = {
   {"あ", "a"},
   {"い", "i"},
   {"う", "e"},
   {"え", "e"},
   {"お", "o"}
 };
+
+function new() {
+  var randomIndex = Math.floor(Math.random() * quizone.length);
+  var question = quizone[randomIndex][0];
+  document.getElementById("spot").innerHTML = question;
+}
