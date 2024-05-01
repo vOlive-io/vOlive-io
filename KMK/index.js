b35fb95322ffde606fc62e4394f20375b15eae72
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-05-01 14:32:56 -0600

    Update index.js

--- KMK/index.js
+++ KMK/index.js
@@ -1,16 +1,16 @@
 //slay
 
 function chooseSet() {
-const characters = ['A duck', 'A cow', 'A sheep', 'A frog', 'A fart', 'A pigeon'];;
-setOne = characters[Math.floor(Math.random() * characters.length)];
-setTwo = characters[Math.floor(Math.random() * characters.length)];
-setThree = characters[Math.floor(Math.random() * characters.length)];
+  const characters = ['A duck', 'A cow', 'A sheep', 'A frog', 'A fart', 'A pigeon'];;
+  setOne = characters[Math.floor(Math.random() * characters.length)];
+  setTwo = characters[Math.floor(Math.random() * characters.length)];
+  setThree = characters[Math.floor(Math.random() * characters.length)];
   //logs
-console.log(setOne)
-console.log(setTwo)
-console.log(setThree)
+  console.log(setOne)
+  console.log(setTwo)
+  console.log(setThree)
   //slot input
-slotOne.innerHTML = setOne;
+  slotOne.innerHTML = setOne;
   slotTwo.innerHTML = setTwo;
   slotThree.innerHTML = setThree;
 }
