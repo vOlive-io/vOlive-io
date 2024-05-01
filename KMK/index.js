f1cca2d65703773cf7354dc9792739f954ac45c3
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-05-01 14:28:51 -0600

    Update index.js

--- KMK/index.js
+++ KMK/index.js
@@ -1,6 +1,11 @@
 //slay
-const texts = ['A duck', 'A cow', 'A sheep', 'A frog'];;
-text = texts[Math.floor(Math.random() * texts.length)];
-console.log(text)
-text = texts[Math.floor(Math.random() * texts.length)];
-console.log(text)
+
+function chooseSet() {
+const characters = ['A duck', 'A cow', 'A sheep', 'A frog'];;
+setOne = characters[Math.floor(Math.random() * characters.length)];
+setTwo = characters[Math.floor(Math.random() * characters.length)];
+setThree = characters[Math.floor(Math.random() * characters.length)];
+console.log(setOne)
+console.log(setTwo)
+console.log(setThree)
+}
