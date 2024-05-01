967b41a9317c44fc60eaee2274a4795d522ec4ec
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-05-01 14:32:40 -0600

    Update index.js

--- KMK/index.js
+++ KMK/index.js
@@ -5,7 +5,12 @@ const characters = ['A duck', 'A cow', 'A sheep', 'A frog', 'A fart', 'A pigeon'
 setOne = characters[Math.floor(Math.random() * characters.length)];
 setTwo = characters[Math.floor(Math.random() * characters.length)];
 setThree = characters[Math.floor(Math.random() * characters.length)];
+  //logs
 console.log(setOne)
 console.log(setTwo)
 console.log(setThree)
+  //slot input
+slotOne.innerHTML = setOne;
+  slotTwo.innerHTML = setTwo;
+  slotThree.innerHTML = setThree;
 }
