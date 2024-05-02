9c351733164f052397b62b1c74e658f11826b90c
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-05-02 07:23:33 -0600

    Update index.js

--- HiraRush/index.js
+++ HiraRush/index.js
@@ -346,12 +346,12 @@ function closecredits() {
 //       EQUIPPING COSMETICS        //
 //////////////////////////////////////
 //basic wallpapers
- function redWall() { if (hiracoins>1) {game.style.backgroundColor = "#ed3737"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
-function oranWall() { if (hiracoins>1) {game.style.backgroundColor = "#ff8800"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
-function yellWall() { if (hiracoins>1) {game.style.backgroundColor = "#fff654"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
-function greeWall() { if (hiracoins>1) {game.style.backgroundColor = "#039600"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
-function blueWall() { if (hiracoins>1) {game.style.backgroundColor = "#759aff"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
-function pinkWall() { if (hiracoins>1) {game.style.backgroundColor = "#ffc4ec"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
+ function redWall() { if (hiracoins>1) {game.style.backgroundColor = "#ed3737"; credits.style.backgroundColor = "#ed3737"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
+function oranWall() { if (hiracoins>1) {game.style.backgroundColor = "#ff8800"; credits.style.backgroundColor = "#ff8800";  hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
+function yellWall() { if (hiracoins>1) {game.style.backgroundColor = "#fff654"; credits.style.backgroundColor = "#fff654"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
+function greeWall() { if (hiracoins>1) {game.style.backgroundColor = "#039600"; credits.style.backgroundColor = "#039600"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
+function blueWall() { if (hiracoins>1) {game.style.backgroundColor = "#759aff"; credits.style.backgroundColor = "#759aff"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
+function pinkWall() { if (hiracoins>1) {game.style.backgroundColor = "#ffc4ec"; credits.style.backgroundColor = "#ffc4ec"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
 
 //basic border
  function redBord() { if (hiracoins>1) {game.style.border = "#ed3737 10px groove"; ach.style.border = "#ed3737 10px groove"; owned.style.border = "#ed3737 10px groove"; credits.style.border = "#ed3737 10px groove"; hiracoins -= 1; switchCount++; update();} else { alert("You need more Hiracoins")}}
