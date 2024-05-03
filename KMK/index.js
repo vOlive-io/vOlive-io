915015055bc388c834259c7f8b9f7aa3578e2bbe
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-05-03 08:44:19 -0600

    Update index.js

--- KMK/index.js
+++ KMK/index.js
@@ -2,8 +2,8 @@ window.addEventListener('load', chooseSet);
 
 
 function chooseSet() {
-//  const characters = ['A duck', 'A cow', 'A sheep', 'A frog', 'A fart', 'A pigeon'];;
-  const characters = ['Callie', 'Marrie', 'Agent 3', 'Agent 4', 'Agent 8', 'Captain 3', 'Craig Cuddlefish', 'DJ Octavio', 'The Great Zapfish', 'O.R.C.A.', 'Pearl', 'Pearl Drone', 'Marina', 'Acht', 'Shiver', 'Frye', 'Big Man', 'Murch', 'Mr. Coco', 'Harmoney', 'Shelldon', ' Jel La Fleur', 'Smallfry', 'Judd', 'Lil Judd', 'Mr. Grizz', 'Staff'];;
+//const characters = ['A duck', 'A cow', 'A sheep', 'A frog', 'A fart', 'A pigeon', 'Trucker', 'Girl off the street', 'Guy off the street', 'Person off the street', 'Gay Stitchs', 'Bob (AC)', 'Lily thr frog queen', 'A living marker', '2 BFDI', 'British guy', 'A fat cat sleeping on a mat', 'Alex', 'Alexa', 'Olive', 'Timmy & Tommy'];;
+const characters = ['Callie', 'Marrie', 'Agent 3', 'Agent 4', 'Agent 8', 'Captain 3', 'Craig Cuddlefish', 'Smallfry', 'Hugefry', 'DJ Octavio', 'The Great Zapfish', 'O.R.C.A.', 'Commander Tartar', 'Pearl', 'Pearl Drone', 'Marina', 'Acht', 'Shiver', 'Frye', 'Big Man', 'Frye\'s eels', 'Master Mega', 'Murch', 'Spyke', 'Mr. Coco', 'Gnarly Eddy', 'Harmony', 'Shelldon', ' Jel La Fleur', 'Judd', 'Lil Judd', 'Mr. Grizz', 'Staff', '(All Of) Deep Cut', '(All Of) Off The Hook', '(All Of) The Squid Sisters', '(All Of) The staff members', 'A random Octarian'];;
   setOne = characters[Math.floor(Math.random() * characters.length)];
   setTwo = characters[Math.floor(Math.random() * characters.length)];
   setThree = characters[Math.floor(Math.random() * characters.length)];
