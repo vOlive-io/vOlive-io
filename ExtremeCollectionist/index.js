cfa1bdccd79290ba1e196cba025c83797478649d
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-05-23 12:41:09 -0600

    Update index.js

--- ExtremeCollectionist/index.js
+++ ExtremeCollectionist/index.js
@@ -1,11 +1,11 @@
-const commonItems = ['pencils','pens','marker', 'cat hair','air','atom','water','fire','playing cards','$1 bill']
-const uncommonItems = ['earthworms (Alive)','earthworms (Dead)','wood','boucey balls','meatballs']
-const rareItems = ['Dial-up phone','Parasol','Wild Neanderthal','Marbles','Plexiglass','Tree','Dirty']
+const commonItems = ['Pencil','Pen','Marker', 'Lump of cat hair','Jar of air','Atom','Water','Fire','Deck of cards','$1 bill']
+const uncommonItems = ['Earthworm (Alive)','Earthworm (Dead)','Wood','Boucey Ball','Meatballs']
+const rareItems = ['Dial-up phone','Parasol','Wild Neanderthal','Marbles','Plexiglass','Tree']
 const epicItems = ['Fart in a jar','Sharktooth', 'Boba Tea Maker','Alphabet blocks']
 const legendaryItems = ['Dragon Scale','Lightsaber','Red Panda Milk','1M pounds of silk']
-const exoticItems = ['Bullet Ant Milk','Golden statue','Dreams']
-const mithicItems = ['diamonds','rhodochrosites','Real Wild Rown','ShophDaBoph']
-const goddlyItems = ['Living Raspberry','your soul','God']
+const exoticItems = ['Bullet Ant Milk','Golden statue','Dreams in a jar']
+const mithicItems = ['Diamond','Rhodochrosite','Real Wild Rown','ShophDaBoph']
+const goddlyItems = ['Living Raspberry','Your soul','God']
 
 commonItemsSlot = commonItems[Math.floor(Math.random() * commonItems.length)];
 uncommonItemsSlot = uncommonItems[Math.floor(Math.random() * uncommonItems.length)];
@@ -16,11 +16,11 @@ exoticItemsSlot = exoticItems[Math.floor(Math.random() * exoticItems.length)];
 mithicItemsSlot = mithicItems[Math.floor(Math.random() * mithicItems.length)];
 goddlyItemsSlot = goddlyItems[Math.floor(Math.random() * goddlyItems.length)];
 
-console.log("You earned a Common: Jar of " + commonItemsSlot)
-console.log("You earned a Uncommon: Jar of " + uncommonItemsSlot)
-console.log("You earned a Rare: Jar of " + rareItemsSlot)
-console.log("You earned a Epic: Jar of " + epicItemsSlot)
-console.log("You earned a Legendary: Jar of " + legendaryitemsSlot)
-console.log("You earned a Exotic: Jar of " + exoticItemsSlot)
-console.log("You earned a Mithic: Jar of " + mithicItemsSlot)
-console.log("You earned a Goddly: Jar of " + goddlyItemsSlot)
+console.log("You earned a Common " + commonItemsSlot)
+console.log("You earned a Uncommon " + uncommonItemsSlot)
+console.log("You earned a Rare " + rareItemsSlot)
+console.log("You earned a Epic " + epicItemsSlot)
+console.log("You earned a Legendary " + legendaryitemsSlot)
+console.log("You earned a Exotic " + exoticItemsSlot)
+console.log("You earned a Mithic " + mithicItemsSlot)
+console.log("You earned a Goddly " + goddlyItemsSlot)
