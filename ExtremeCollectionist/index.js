2806c6829ecfdb8083acfbae148683b711db334c
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2024-05-23 10:41:39 -0600

    Update index.js

--- ExtremeCollectionist/index.js
+++ ExtremeCollectionist/index.js
@@ -1,8 +1,18 @@
 const commonItems = ['','']
 const uncommonItems = ['','']
 const rareItems = ['','']
-const epiccommonItems = ['','']
+const epicItems = ['','']
 const legendaryItems = ['','']
 const exoticItems = ['','']
 const mithicItems = ['','']
 const goddlyItems = ['','']
+
+
+console.log("You earned a Common " + commonItems)
+console.log("You earned a Uncommon " + uncommonItems)
+console.log("You earned a Rare " + rareItems)
+console.log("You earned a Epic " + epicItems)
+console.log("You earned a Legendary " + legendaryItems)
+console.log("You earned a Exotic " + exoticItems)
+console.log("You earned a Mithic " + mithicItems)
+console.log("You earned a Goddly " + goddlyItems)
