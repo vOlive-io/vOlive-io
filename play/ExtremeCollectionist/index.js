40a9ee195b90b1b018d7dd610ec66ec1964b1851
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-09-07 15:50:11 -0600

    Update index.js
    omg vs is like really good...

--- play/ExtremeCollectionist/index.js
+++ play/ExtremeCollectionist/index.js
@@ -1,25 +1,55 @@
 var commonItems = [
-  ["Pencil", "Item Description", 0],
-  ["Pen", "Item Description", 0],
-  ["Marker", "Item Description", 0],
-  ["Lump of cat hair", "Item Description", 0],
-  ["Jar of air", "Item Description", 0],
-  ["Atom", "Item Description", 0],
-  ["Water", "Item Description", 0],
-  ["Fire", "Item Description", 0],
-  ["Deck of cards", "Item Description", 0],
-  ["$1 bill", "Item Description", 0],
-  ["Flyswatter", "Item Description", 0],
-  ["Snowglobe", "Item Description", 0],
-  ["Toilet", "Item Description", 0]
+  ["1.1.1.1", "Pencil", "Item Description", "Collection Name", 0],
+  ["1.1.1.2", "Pen", "Item Description", "Collection Name", 0],
+  ["1.1.1.3", "Marker", "Item Description", "Collection Name", 0],
+  ["1.1.1.4", "Lump of cat hair", "Item Description", "Collection Name", 0],
+  ["1.1.1.5", "Jar of air", "Item Description", "Collection Name", 0],
+  ["1.1.1.6", "Deck of cards", "Item Description", "Collection Name", 0],
+  ["1.1.1.7", "$1 bill", "Item Description", "Collection Name", 0],
+  ["1.1.1.8", "Flyswatter", "Item Description", "Collection Name", 0],
+  ["1.1.1.9", "Snowglobe", "Item Description", "Collection Name", 0],
+  ["1.1.1.10", "Toilet", "Item Description", "Collection Name", 0]
 ];
-
-const uncommonItems = ["Earthworm (Alive)","Earthworm (Dead)","Wood","Boucey Ball","Meatballs","Rubber Duck","Mouse Pad","Salt Shaker","Pepper Shaker"]
-const rareItems = ["Dial-up phone","Parasol","Wild Neanderthal","Marble","Plexiglass","Tree"]
-const epicItems = ["Fart in a jar","Sharktooth", "Boba Tea Maker","Alphabet blocks"]
-const legendaryItems = ["Dragon Scale","Lightsaber","Red Panda Milk","1M pounds of silk"]
-const mythicItems = ["Diamond","Rhodochrosite"]
-const goddlyItems = ["","Your soul","God"]
+var uncommonItems = [
+  ["1.1.2.1", "Earthworm (Alive)", "Item Description", "Collection Name", 0],
+  ["1.1.2.2", "Earthworm (Dead)", "Item Description", "Collection Name", 0],
+  ["1.1.2.3", "Wood", "Item Description", "Collection Name", 0],
+  ["1.1.2.4", "Boucey Ball", "Item Description", "Collection Name", 0],
+  ["1.1.2.5", "Meatballs", "Item Description", "Collection Name", 0],
+  ["1.1.2.6", "Rubber Duck", "Item Description", "Collection Name", 0],
+  ["1.1.2.7", "Mouse Pad", "Item Description", "Collection Name", 0],
+  ["1.1.2.8", "Salt Shaker", "Item Description", "Collection Name", 0],
+  ["1.1.2.9", "Pepper Shaker", "Item Description", "Collection Name", 0]
+];
+var rareItems = [
+  ["1.1.3.1", "Dial-up phone", "Item Description", "Collection Name", 0],
+  ["1.1.3.2", "Parasol", "Item Description", "Collection Name", 0],
+  ["1.1.3.3", "Wild Neanderthal", "Item Description", "Collection Name", 0],
+  ["1.1.3.4", "Marble", "Item Description", "Collection Name", 0],
+  ["1.1.3.5", "Plexiglass", "Item Description", "Collection Name", 0],
+  ["1.1.3.6", "Tree", "Item Description", "Collection Name", 0]
+];
+var epicItems = [
+  ["1.1.4.1", "Fart in a jar", "Item Description", "Collection Name", 0],
+  ["1.1.4.2", "Sharktooth", "Item Description", "Collection Name", 0],
+  ["1.1.4.3", "Boba Tea Maker", "Item Description", "Collection Name", 0],
+  ["1.1.4.4", "Alphabet blocks", "Item Description", "Collection Name", 0]
+];
+var legendaryItems = [
+  ["1.1.5.1", "Dragon Scale", "Item Description", "Collection Name", 0],
+  ["1.1.5.2", "Lightsaber", "Item Description", "Collection Name", 0],
+  ["1.1.5.3", "Red Panda Milk", "Item Description", "Collection Name", 0],
+  ["1.1.5.4", "1M pounds of silk", "Item Description", "Collection Name", 0]
+];
+var mythicItems = [
+  ["1.1.6.1", "Diamond", "Item Description", "Collection Name", 0],
+  ["1.1.6.2", "Rhodochrosite", "Item Description", "Collection Name", 0]
+];
+var goddlyItems = [
+  ["1.1.7.1", "Item Description", "Collection Name", 0],
+  ["1.1.7.2", "Your soul", "Item Description", "Collection Name", 0],
+  ["1.1.7.3", "God", "Item Description", "Collection Name", 0]
+];
 
 var money = 5.00;
 var rollCost = 1.50;
