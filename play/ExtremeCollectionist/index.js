cd0ad3a15e4fd47e14cfc09d712d70cd3b5455d6
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-09-07 15:57:24 -0600

    Update index.js

--- play/ExtremeCollectionist/index.js
+++ play/ExtremeCollectionist/index.js
@@ -46,12 +46,13 @@ var commonItems = [
   ["1.1.1.45", "Item name", "Item Description", "Collection Name", 0],
   ["1.1.1.46", "Item name", "Item Description", "Collection Name", 0],
   ["1.1.1.47", "Item name", "Item Description", "Collection Name", 0],
-  ["1.1.1.47", "$1 bill", "Item Description", "Collection Name", 0],
+  ["1.1.1.47", "$1 bill", "Item Description", "Money", 0],
   ["1.1.1.48", "Flyswatter", "Item Description", "Collection Name", 0],
   ["1.1.1.49", "Snowglobe", "Item Description", "Collection Name", 0],
   ["1.1.1.50", "Toilet", "Item Description", "Collection Name", 0]
 ];
 var uncommonItems = [
+  ["1.1.2.1", "Earthworm (Alive)", "Item Description", "Collection Name", 0],
   ["1.1.2.1", "Earthworm (Alive)", "Item Description", "Collection Name", 0],
   ["1.1.2.2", "Earthworm (Dead)", "Item Description", "Collection Name", 0],
   ["1.1.2.3", "Wood", "Item Description", "Collection Name", 0],
@@ -63,6 +64,7 @@ var uncommonItems = [
   ["1.1.2.9", "Pepper Shaker", "Item Description", "Collection Name", 0]
 ];
 var rareItems = [
+  ["1.1.3.1", "$5 Bill", "Item Description", "Money", 0],
   ["1.1.3.1", "Dial-up phone", "Item Description", "Collection Name", 0],
   ["1.1.3.2", "Parasol", "Item Description", "Collection Name", 0],
   ["1.1.3.3", "Wild Neanderthal", "Item Description", "Collection Name", 0],
@@ -71,23 +73,26 @@ var rareItems = [
   ["1.1.3.6", "Tree", "Item Description", "Collection Name", 0]
 ];
 var epicItems = [
+  ["1.1.4.1", "$10 Bill", "Item Description", "Money", 0],
   ["1.1.4.1", "Fart in a jar", "Item Description", "Collection Name", 0],
   ["1.1.4.2", "Sharktooth", "Item Description", "Collection Name", 0],
   ["1.1.4.3", "Boba Tea Maker", "Item Description", "Collection Name", 0],
   ["1.1.4.4", "Alphabet blocks", "Item Description", "Collection Name", 0]
 ];
 var legendaryItems = [
+  ["1.1.5.1", "$50 Bill", "Item Description", "Money", 0],
   ["1.1.5.1", "Dragon Scale", "Item Description", "Collection Name", 0],
   ["1.1.5.2", "Lightsaber", "Item Description", "Collection Name", 0],
   ["1.1.5.3", "Red Panda Milk", "Item Description", "Collection Name", 0],
   ["1.1.5.4", "1M pounds of silk", "Item Description", "Collection Name", 0]
 ];
 var mythicItems = [
+  ["1.1.6.1", "$100 Bill", "Item Description", "Money", 0],
   ["1.1.6.1", "Diamond", "Item Description", "Collection Name", 0],
   ["1.1.6.2", "Rhodochrosite", "Item Description", "Collection Name", 0]
 ];
 var goddlyItems = [
-  ["1.1.7.1", "Item Description", "Collection Name", 0],
+  ["1.1.7.1", "$∞ Dollar Bill", "Collection Name", 0],
   ["1.1.7.2", "Your soul", "Item Description", "Collection Name", 0],
   ["1.1.7.3", "God", "Item Description", "Collection Name", 0]
 ];
